9f3f06a0fafbe495ea0f3f6a47b03e60377d06af
tukY Chyvekshyno <onar2000@gmail.com>
2020-07-11 19:41:10 +0300

    Change button to FlaotingActionButton; Add new activity for adding essences; set onClick method for fab

---
+++ app/src/main/java/com/example/tukyhelper/View/EssenceAddActivity.java
@@ -0,0 +1,16 @@
+package com.example.tukyhelper.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tukyhelper.R;
+
+public class EssenceAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_essence_add);
+    }
+}
--- app/src/main/java/com/example/tukyhelper/View/MainActivity.java
+++ app/src/main/java/com/example/tukyhelper/View/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 
 import com.example.tukyhelper.Model.EssenceRoom.Essence;
 import com.example.tukyhelper.Model.EssenceRoom.EssenceType;
 import com.example.tukyhelper.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -21,15 +24,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create tested Essences
         ArrayList<Essence> ess_list = new ArrayList<>();
         ess_list.add(new Essence(1, 1, "Home", "_", 0));
         ess_list.add(new Essence(2, 1, "Home2", "_", 0));
         ess_list.add(new Essence(4, 1, "Home3", "_", 0));
 
+        // Set adapter to RecycleView
         rv_essence_list = (RecyclerView) findViewById(R.id.rv_essence_list);
         rv_essence_list.setLayoutManager(new GridLayoutManager(this, 3));
         EssenceRVAdapter adapter = new EssenceRVAdapter();
         adapter.setEssences(ess_list);
         rv_essence_list.setAdapter(adapter);
+
+        // set onClick for fab
+        FloatingActionButton fab_add = (FloatingActionButton) findViewById(R.id.fab_essence_add);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EssenceAddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
