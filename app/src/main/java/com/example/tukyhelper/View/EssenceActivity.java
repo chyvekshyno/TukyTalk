61727223a75958b965789f8c633c74ac4aa54dd3
tukY Chyvekshyno <onar2000@gmail.com>
2020-07-18 19:51:10 +0300

    Add new Activity for essence view

---
+++ app/src/main/java/com/example/tukyhelper/View/EssenceActivity.java
@@ -0,0 +1,19 @@
+package com.example.tukyhelper.View;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tukyhelper.R;
+
+public class EssenceActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.essence_activity);
+    }
+}
