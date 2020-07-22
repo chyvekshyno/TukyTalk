1232f7be47e326d48f2def8e75b7d6d339cd2d2c
tukY Chyvekshyno <onar2000@gmail.com>
2020-07-22 14:56:41 +0300

    add tab layout gor messages in essence layout

---
+++ app/src/main/java/com/example/tukyhelper/View/Adapters/MessageVPAdapter.java
@@ -0,0 +1,28 @@
+package com.example.tukyhelper.View.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MessageVPAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragmentList;
+
+    public MessageVPAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
---
+++ app/src/main/java/com/example/tukyhelper/View/Fragments/MessageFragment.java
@@ -0,0 +1,31 @@
+package com.example.tukyhelper.View.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tukyhelper.R;
+
+public class MessageFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.essence_msg_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
